dfe08245ea3346e85e57e5ebb8f26eaa4546d930
Joshua Wolfson <J.a.wolfson@icloud.com>
2025-04-18 09:56:47 +1000

    added base path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    basePath: "/onCue/converter",
+    output: "export",
 };
 
 export default nextConfig;
